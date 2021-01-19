cb4b3a0b0ca62f67b99011ae65a0eb79f48a0719
Laurens Walraeve <laurens.walraeve@student.howest.be>
2021-01-19 17:31:40 +0100

    CustomEntry & CustomEntryRenderer

---
+++ TeamProject/TeamProject.Android/CustomEntryRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using TeamProject.Models;
+using TeamProject.Droid;
+using Android.Graphics;
+
+#pragma warning disable CS0436 // Type conflicts with imported type
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+#pragma warning restore CS0436 // Type conflicts with imported type
+namespace TeamProject.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
